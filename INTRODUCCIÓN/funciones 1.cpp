fa9793b96ae41d128f769eb3a158144597bb8c70
jonathan210801 <79435934+jonathan210801@users.noreply.github.com>
2021-03-09 15:56:45 -0600

    Add files via upload

---
+++ INTRODUCCIÓN/funciones 1.cpp
@@ -0,0 +1,19 @@
+//Celiseo Morales Jonathan Eduardo
+// CALCULE EL AREA DE UN RECTANGULO PROBLEMA 1 FUNCIONES
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    float base;
+    float altura;
+    cout << "calculo el area de un rectangulo" << endl;
+    cout << "Introduzca la base: "<< endl;
+    cin >>base;
+    cout << "Introduzca la altura:";
+    cin >>altura;
+    float area= base*altura;
+    cout << "El area es: " <<area << endl;
+}
